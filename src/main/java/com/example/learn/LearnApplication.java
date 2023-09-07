3d53fa21bcc37abb8de6b8dc19060b03a5df2ed4
Mehdi Ebrahimi <mehdi.ebrahimi2782@gmail.comi>
2023-09-07 10:53:30 +0330

    add  swagger

--- src/main/java/com/example/learn/LearnApplication.java
+++ src/main/java/com/example/learn/LearnApplication.java
@@ -1,8 +1,13 @@
 package com.example.learn;
 
+import com.example.learn.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({
+        SwaggerConfig.class,
+})
 @SpringBootApplication
 public class LearnApplication {
 
--- src/main/java/com/example/learn/controller/ProductController.java
+++ src/main/java/com/example/learn/controller/ProductController.java
@@ -2,26 +2,34 @@ package com.example.learn.controller;
 
 import com.example.learn.entity.Product;
 import com.example.learn.service.ProductService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Product", description = "defines endpoints for Product")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/product")
+@RequiredArgsConstructor
+@Validated
+@Log4j2
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping(value = {"/getProduct"})
+    @GetMapping(value = {"/get"})
     public List<Product> getAllCheckOutStatusBankTransferLimited() {
 
        return productService.getProduct();
     }
 
 
-    @PostMapping
+    @PostMapping("/add")
     public Product addProduct(@PathVariable Product product){
         return productService.addProduct(product);
     }
