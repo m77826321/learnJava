33206eed837cf3ed3c77cf80163a47a9e674ee46
Mehdi Ebrahimi <mehdi.ebrahimi2782@gmail.comi>
2023-09-27 16:14:21 +0330

    improve cache 1

--- src/main/java/com/example/learn/service/ProductService.java
+++ src/main/java/com/example/learn/service/ProductService.java
@@ -2,6 +2,7 @@ package com.example.learn.service;
 
 import com.example.learn.entity.Product;
 import com.example.learn.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -22,13 +23,14 @@ public class ProductService {
         return lst;
     }
 
-    @Cacheable(cacheNames = "product-cache" ,key="#id")
+    @Cacheable(cacheNames = "product-cache", key = "#id")
     public Product getProduct(Long id) {
         var product = productRepository.findById(id).get();
         return product;
     }
 
-    @CacheEvict(cacheNames ="product-cache",key = "#product.id")
+    @Transactional
+    @CacheEvict(cacheNames = "product-cache", key = "#product.id")
     public Product addProduct(Product product) throws Exception {
         if (product.getName().length() > 5) {
             throw new Exception("Sorry Add");
